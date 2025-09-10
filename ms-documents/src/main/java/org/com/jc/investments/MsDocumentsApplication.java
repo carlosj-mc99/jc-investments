04ef8f6ef040a4997cfdcf50cceb1dbddda6183f
Carlos <Carlos@LAPTOP-GVH1B2DI>
2025-09-09 19:02:08 -0600

    prueba commit ms-documents

---
+++ ms-documents/src/main/java/org/com/jc/investments/MsDocumentsApplication.java
@@ -0,0 +1,13 @@
+package org.com.jc.investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDocumentsApplication.class, args);
+	}
+
+}
