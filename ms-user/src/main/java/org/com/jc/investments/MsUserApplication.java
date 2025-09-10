658f9a70844338fe0789295b0cdcaaddeb170c20
Carlos <Carlos@LAPTOP-GVH1B2DI>
2025-09-09 19:02:33 -0600

    prueba commit ms-user

---
+++ ms-user/src/main/java/org/com/jc/investments/MsUserApplication.java
@@ -0,0 +1,13 @@
+package org.com.jc.investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserApplication.class, args);
+	}
+
+}
