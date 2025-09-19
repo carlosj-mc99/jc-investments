291bfc75d7ff4b9f0efd80066534965b6c25e250
carlosjmc <carlosmc828@gmail.com>
2025-09-18 21:37:56 -0600

    configuracion de ms-transaction y del gateway

---
+++ ms-transactions/src/main/java/org/com/jc/investments/MsTransactionsApplication.java
@@ -0,0 +1,13 @@
+package org.com.jc.investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransactionsApplication.class, args);
+	}
+
+}
